099d9c6aa0a84eb933d6d42800a70b0295002d6c
johnmuninga <johnmuninga03@gmail.com>
2024-05-08 14:45:58 +0200

    Your commit message here

---
+++ If-Else.py
@@ -0,0 +1,8 @@
+i = 20
+if (i< 15):
+    print("i is less than 15")
+    print("i am in if block")
+else:
+    print("i is greatet than 15")
+    print("I am in else block")
+print("i am not in if and not in else Block")
